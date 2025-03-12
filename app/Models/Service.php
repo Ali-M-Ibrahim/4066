8bef5a1822705baefa53208ed6707066dfb6067b
ali <ali-ibrahim94@hotmail.com>
2025-03-12 17:31:25 +0200

    add

---
+++ app/Models/Service.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    public function getCustomers(){
+        return $this->belongsToMany(
+            Customer::class,
+            "customer_services",
+            "service_id",
+            "customer_id"
+        );
+    }
+}
